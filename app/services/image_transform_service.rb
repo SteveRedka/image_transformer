242ef52a54579dafb8a0aa733565f59918c867b6
Stepan Redka <cogitofallacy@gmail.com>
2019-12-14 17:49:10 +0300

    refactor: better naming

--- app/services/image_transform_service.rb
+++ app/services/image_transform_service.rb
@@ -1,16 +1,16 @@
 require 'mini_magick'
 
 class ImageTransformService
-  def initialize(input_path, params = '')
+  def initialize(input_path, transformations = '')
     @input_path = input_path
-    @params = params.split(' ')
+    @transformations = transformations.split(' ')
     @image = MiniMagick::Image.open(@input_path).dup
   end
 
   def call
     MiniMagick::Tool::Convert.new do |convert|
       convert << @input_path
-      convert.merge! @params
+      convert.merge! @transformations
       convert << @image.path
     end
     @image
